73f2df2029d084d5ec5fccf3f15576d4e8969625
rodrosoftvence22 <rodrosoftvence@gmail.com>
2025-03-22 14:37:03 +0600

    first

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+
+    const mobilenav = () => {
+        let wrapper = document.querySelector('.navrapper')
+
+        if(wrapper){
+
+            // Code goes here
+        }
+    }
+
+    mobilenav()
+    
+    const footerLink = () => {
+        let wrapper = document.querySelector('.footer')
+
+        if(wrapper){
+
+            // Code goes here
+
+        }
+    }
+
+    footerLink()
+
+   
+    
+
+})
